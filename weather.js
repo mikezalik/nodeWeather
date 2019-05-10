a52b65b994df2d0274544cc7d25ceb2bbac509a1
mikezalik <mike.zalik@gmail.com>
2019-05-09 19:38:11 -0700

    refactor

--- weather.js
+++ weather.js
@@ -15,49 +15,49 @@ function printError(error) {
 }
   
 function get(query) {
-    try {
-        const parameters = {
-            APPID: api.key,
-            units: 'imperial'
-        };
+  try {
+    const parameters = {
+      APPID: api.key,
+      units: 'imperial'
+  };
 
-    const zipCode = parseInt(query);
+  const zipCode = parseInt(query);
     if (!isNaN(zipCode)) {
       parameters.zip = zipCode + ',us';
     } else {
       parameters.q = query + ',us';
-    }
+  }
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(parameters)}`;
-    console.log(url);
+  const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(parameters)}`;
+  console.log(url);
 
-    const request = https.get(url, response => {
-        if (response.statusCode === 200) {
-            let body = '';
-            // Read the data
-            response.on('data', chunk => {
-              body += chunk;
-            });
-            response.on('end', () => {
-              try {
-                //Parse data
-                const weather = JSON.parse(body);
-                //Print the data
-                printWeather(weather);
-              } catch (error) {
-                //Parser error
-                printError(error);
-              }
-            });
-          } else {
-            // Status error code
-            const statusErrorCode = new Error(`There was an error getting the message for "${query}". (${http.STATUS_CODES[response.statusCode]})`);
-            printError(statusErrorCode);
-          }
-        });      
-} catch (error) {
-    printError(error);
-  } 
+  const request = https.get(url, response => {
+    if (response.statusCode === 200) {
+      let body = '';
+// Read the data
+      response.on('data', chunk => {
+        body += chunk;
+      });
+      response.on('end', () => {
+        try {
+//Parse data
+      const weather = JSON.parse(body);
+//Print the data
+      printWeather(weather);
+        } catch (error) {
+//Parser error
+          printError(error);
+        }
+      });
+    } else {
+// Status error code
+      const statusErrorCode = new Error(`There was an error getting the message for "${query}". (${http.STATUS_CODES[response.statusCode]})`);
+        printError(statusErrorCode);
+        }
+  });      
+  } catch (error) {
+      printError(error);
+    } 
 }
 
 module.exports.get = get;
